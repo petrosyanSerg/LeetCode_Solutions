bc31ee20d4cafab5700f93ca3fffa8f0d7a6d51d
Sergey Petrosyan <petrosyanserg33@gmail.com>
2025-01-06 14:59:50 +0400

    20. Valid Parentheses

---
+++ Easy/20_Valid_Parentheses.js
@@ -0,0 +1,52 @@
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+Example 1:
+  Input: s = "()"
+  Output: true
+
+Example 2:
+  Input: s = "()[]{}"
+  Output: true
+
+Example 3:
+  Input: s = "(]"
+  Output: false
+
+Example 4:
+  Input: s = "([])"
+  Output: true
+ */
+
+function isValid(str) {
+  const stack = [];
+  const mapping = {
+    ')': '(',
+    '}': '{',
+    ']': '['
+  };
+
+  for (const bracket of str) {
+    if (Object.values(mapping).includes(bracket)) {
+      stack.push(bracket);
+    } else if (!!mapping[bracket]) {
+      if (!stack.length || mapping[bracket] !== stack.pop()) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
+
+console.log(isValid('()'))
+console.log(isValid('()[]{}{}'))
+console.log(isValid('([])'))
+console.log(isValid('([)]'))
